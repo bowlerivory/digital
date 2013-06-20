2577d5d8acf74d866ac49fb100bd77bad35fb82c
bowlerivory <shaunaksai@gmail.com>
2013-06-20 11:18:23 -0600

    Update LaserTripWireSensor.ino

--- digitalIntro/Theremin/LaserTripWireSensor.ino
+++ digitalIntro/Theremin/LaserTripWireSensor.ino
@@ -1,11 +1,20 @@
-
-
 void setup() {
  Serial.begin(9600);
 }
 
 void loop() {
   int sensorReading = analogRead(A0);
+  itn midPoint = 250;
+  if (sensorReading > 250)
+  { 
+   Serial.println("Everything's chill; Party on!");
+   }
+  else
+  {
+  Serial.println("Alert -- release the drones!");
+  
+  }
+  
   Serial.println(sensorReading);
   delay(1);        
 }
